694d01a92828269cc1b601ff83e69a7639e72a17
AjaySinghRandhawa <arandhaw@deakin.edu.au>
2023-03-13 18:36:02 +1100

    commit 2
    Signed-off-by: AjaySinghRandhawa <arandhaw@deakin.edu.au>

---
+++ week2server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+let users = [
+  { id: 1, name: 'John' },
+  { id: 2, name: 'Jane' },
+  { id: 3, name: 'Bob' }
+];
+
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+app.post('/users', (req, res) => {
+  const newUser = req.body;
+  users.push(newUser);
+  res.status(201).json(newUser);
+});
+
+app.put('/users/:id', (req, res) => {
+  const userId = parseInt(req.params.id);
+  const updatedUser = req.body;
+  users = users.map(user => user.id === userId ? updatedUser : user);
+  res.status(200).json(updatedUser);
+});
+
+app.delete('/users/:id', (req, res) => {
+  const userId = parseInt(req.params.id);
+  users = users.filter(user => user.id !== userId);
+  res.status(204).send();
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
